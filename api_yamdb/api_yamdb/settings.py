f6ca05539b35edfd26b9947dd22ad5d36bfdcb3f
Глеб Останин <gleb.ostanin2014@yandex.ru>
2023-03-28 19:12:43 +0600

    Setting frame

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,6 +21,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
+    'posts.apps.PostsConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
