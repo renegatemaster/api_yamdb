98239786d626df622510a821e4caebfa3af7f3bc
sergejsarkisan <sar.ser.gen@yandex.ru>
2023-03-29 17:07:02 +0300

    Done test User, registration

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from .validators import UsernameValidator
+
+
+class User(AbstractUser):
+    USER = "user"
+    MODERATOR = "moderator"
+    ADMIN = "admin"
+
+    roles = (
+        (USER, USER),
+        (MODERATOR, MODERATOR),
+        (ADMIN, ADMIN),
+    )
+    username_validator = UsernameValidator()
+    username = models.CharField(
+        "Имя пользователя",
+        max_length=150,
+        unique=True,
+        validators=[username_validator],
+    )
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    email = models.EmailField("Email", max_length=254, unique=True)
+    role = models.CharField(
+        "Роль пользователя", choices=roles, max_length=255, default=USER
+    )
+    bio = models.TextField("Биография", blank=True)
+    confirmation_code = models.CharField("Код подтверждения",
+                                         max_length=100, null=True)
+
+    REQUIRED_FIELDS = ["email"]
+    USERNAME_FIELDS = "email"
+
+    def __str__(self):
+        return str(self.username)
+
+    @property
+    def is_admin(self):
+        return self.role == self.ADMIN
+
+    @property
+    def is_moderator(self):
+        return self.role == self.MODERATOR
+
+    @property
+    def is_user(self):
+        return self.role == self.USER
